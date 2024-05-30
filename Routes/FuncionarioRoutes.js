e622cfc8fcc861e0b30cf156f7b309aff797f3b1
BrendaMarini <brendalauramarini@gmail.com>
2024-05-30 17:04:01 -0300

    first commit

---
+++ Routes/FuncionarioRoutes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const Funcionario = require('../models/Funcionario');
+
+
+router.post('/', async (req, res) => {
+    const { nome, cargo, salario, desligado } = req.body;
+    if (!nome || !cargo || !salario || desligado === undefined) {
+        return res.status(422).json({ error: 'Informar o nome, cargo, salario e desligado é obrigatório!' });
+    }
+    
+    const funcionario = {
+        nome,
+        cargo,
+        salario,
+        desligado,
+    };
+
+    try {
+        await Funcionario.create(funcionario);
+        res.status(201).json({ message: 'Funcionário cadastrado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+//GET
+router.get('/:id?', async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        if (id) {
+            const funcionario = await Funcionario.findById(id);
+            if (!funcionario) {
+                return res.status(404).json({ error: 'Funcionário não encontrado!' });
+            }
+            return res.status(200).json(funcionario);
+        } else {
+            const funcionarios = await Funcionario.find();
+            res.status(200).json(funcionarios);
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, cargo, salario, desligado } = req.body;
+
+    
+    if (!nome && !cargo && !salario && desligado === undefined) {
+        return res.status(422).json({ error: 'Informar ao menos um campo para atualização!' });
+    }
+
+    try {
+        const updatedFuncionario = await Funcionario.findByIdAndUpdate(id, { nome, cargo, salario, desligado }, { new: true, runValidators: true });
+        if (!updatedFuncionario) {
+            return res.status(404).json({ error: 'Funcionário não encontrado!' });
+        }
+        res.status(200).json({ message: 'Funcionário atualizado com sucesso', funcionario: updatedFuncionario });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// DELETE 
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedFuncionario = await Funcionario.findByIdAndDelete(id);
+        if (!deletedFuncionario) {
+            return res.status(404).json({ error: 'Funcionário não encontrado!' });
+        }
+        res.status(200).json({ message: 'Funcionário deletado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+module.exports = router;
+
+
---
+++ index.js
@@ -0,0 +1,155 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const server = express();
+
+
+const funcionarioRoutes = require('./Routes/FuncionarioRoutes')
+
+//Middleware
+server.use(
+    express. urlencoded({
+    extended: true,
+    }),
+);
+
+server.use(express.json());
+
+server.use('/funcionario', funcionarioRoutes);
+
+const DB_USER ='brendalauramarini';
+const DB_PASSWORD = encodeURIComponent('mZHs26dVOAySWMhp')
+//Cpnexao com mongoDB atlas
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xrbeuvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(()=>{
+        console.log('Conectado ao MongoDB!');
+    })
+    .catch((err)=>{
+        console. log(err);
+    })
+
+server.listen(3000);
+
+/*const express = require('express');
+const server = express();
+server.use(express.json());
+
+const cursos = ['Node JS', 'Javascript', 'PHP', 'React show'];
+
+server.use((req, res, next) =>{
+    console.log(`URL CHAMADA: ${req.url}`);
+    return next();
+});
+
+function checkCurso(req, res, next){
+    if(!req.body.novo_curso){
+        return res.status(400).json({error:
+        "Nome do curso é obrigatório nesse formato {'novo_nome': 'Lua'}"});
+    }
+
+    return next();
+}
+
+server.get('/curso',(req, res)=>{
+    return res.json(cursos);
+});
+
+function checkIDCurso(req, res, next){
+    const curso = cursos[req.params.index];
+    if(!curso){
+        return res.status(400).json({ error: "O curso não existe no ID solicitado"});
+    }
+
+    return next();
+}
+
+server.get('/curso/:index', checkIDCurso, (req, res) => {
+    const {index} = req.params;
+    return res.json(cursos[index]);
+});
+
+function checkRequestBodyPost(req, res, next) {
+    if (req.method === 'POST' && !req.body.novo_curso) {
+        return res.status(400).json({ error: 
+            "O corpo da requisição POST não pode estar vazio. Use o formato {'novo_curso': 'Nome do Curso'}" });
+    }
+    next();
+}
+
+server.post('/curso',  logCursos, checkRequestBodyPost, (req, res)=>{
+    const {novo_curso} = req.body;
+    cursos.push(novo_curso);
+
+    return res.json(cursos);
+});
+
+function checkRequestBody(req, res, next) {
+    if (req.method === 'PUT' && Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: "O corpo da requisição PUT não pode estar vazio" });
+    }
+    next();
+}
+
+server.put('/curso/:index', checkIDCurso, checkRequestBody, (req, res)=>{
+    const { index } = req.params;
+    const { curso } = req.body;
+
+    cursos[index] = curso;
+
+    return res.json(cursos);
+});
+
+function handleDeleteError(req, res, next) {
+    const { index } = req.params;
+
+    if (index < 0 || index >= cursos.length) {
+        return res.status(404).json({ error: "Índice inválido, curso não encontrado." });
+    }
+
+    next();
+}
+
+function deleteCurso(req, res) {
+    const { index } = req.params;
+
+    const deletedCurso = cursos.splice(index, 1);
+
+    if (deletedCurso.length === 0) {
+        return res.status(500).json({ error: "Erro ao excluir o curso." });
+    }
+
+    return res.json({ message: "Curso deletado com sucesso" });
+}
+
+function logCursoDeletado(req, res, next) {
+    const { index } = req.params;
+
+    const cursosAntes = [...cursos];
+    next();
+
+    if (res.statusCode === 200) {
+        console.log(`Curso deletado: ${cursosAntes[index]}`);
+        console.log("Lista Anterior: ",cursosAntes);
+        console.log("Lista de cursos atualizada:");
+        console.log(cursos);
+    }
+}
+
+server.delete('/curso/:index', handleDeleteError, logCursoDeletado, deleteCurso);
+
+function logCursos(req, res, next) {
+    const cursosAntes = [...cursos];
+    next();
+    console.log("Lista de cursos atualizada:");
+    console.log(cursosAntes);
+    console.log(cursos);
+}
+
+server.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+});
+
+server.listen(3000);
+*/
